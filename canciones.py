ae920b3e5b12afe17666e5cc4cfcbf133065e8ba
Octavio Sanchez <octavio@gacs.mx>
2019-08-15 17:13:10 -0500

    analisis estadístico y creación de tokens

--- canciones.py
+++ canciones.py
@@ -3,10 +3,29 @@ import os
 import requests
 import pandas as pd
 
-#%%
-df = pd.read_csv("lyrics/billboard_lyrics_1964-2015.csv", encoding="cp1252")
+data = pd.read_csv("https://raw.githubusercontent.com/walkerkq/musiclyrics/master/billboard_lyrics_1964-2015.csv", encoding="cp1252")
+con_letra = data[data["Lyrics"].notnull()]
+con_letra["Length"] = con_letra.Lyrics.apply(len)
 
+# Determinar estadísticas
+longitud = con_letra.Length
+num_dev = 1
+mean = longitud.mean()
+std = longitud.std()
+theta = int(mean - (num_dev * std))
+con_letra = con_letra[con_letra["Length"] > theta]
 #%%
-con_letras = df[df["Lyrics"].notnull()]
-
-#%%
+def tokenize(cadena):
+    tokens = cadena.split()
+    retoks = []
+    for token in tokens:
+        retok = ""
+        for char in token:
+            if char.isalpha():
+                retok += char
+            else:
+                if retok:
+                    retoks.append(retok)
+                    retok = ""
+                retoks.append(char)
+    return retoks
